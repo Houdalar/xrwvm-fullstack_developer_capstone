1769e4ee8f83a1eb6de74917ba37c16171a90c7f
Houdalar <houda.lariani@esprit.tn>
2024-04-04 03:41:25 -0400

    week 2 Django Proxy Services

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,10 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.registration, name='register'),
     # path for dealer reviews view
-
-    # path for add a review view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='get_dealer_details'),
+    path('reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews, name='get_dealer_reviews'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
